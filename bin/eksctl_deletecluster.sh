80de47d045289555ea0446f4d12d615eb83b0e4a
Peter Killick <peter.killick@metoffice.gov.uk>
2019-03-14 16:28:48 +0000

    AWS assume role for scripts

--- bin/eksctl_deletecluster.sh
+++ bin/eksctl_deletecluster.sh
@@ -1,27 +1,25 @@
-# Script to delete the Pangeo EKS Cluster.
+#!/usr/bin/env bash
+
+# Script to delete the EKS Cluster.
 
 set -ex
+source "functions.sh"
+
+
+# Call the `assume_role` function from `functions.sh`.
+assume_role
 
 # Get the names of things to be deleted.
 CLUSTER_NAME=$(eksctl get cluster | grep -iv name | awk '{ print $1 }')
-CUSTOMISATION_STACK_NAME="eksctl-$CLUSTER_NAME-customisations"
+CUSTOMISATION_STACK_NAME="$CLUSTER_NAME-customisations"
 
-# Delete the loadbalancer set up by nginx-ingress.
-helm delete --purge nginx-ingress
+# List all helm charts and delete them one by one.
+CHART_NAMES=$(helm list | grep -iv name | awk '{ print $1 }')
+echo $CHART_NAMES
 
-# Delete other helm charts.
-
-# [Placeholder for spot integration (?)]
-# [Placeholder for dashboard service]
-helm delete --purge cluster-autoscaler
-# [Placeholder for Prometheus/Grafana]
-# [Placeholder for cloudwatch]
-helm delete --purge cert-manager
-helm delete --purge external-dns
-helm delete --purge kube2iam
-helm delete --purge nginx-ingress
-helm delete --purge s3-fuse-deployer
-helm delete --purge efs-provisioner
+for CHART_NAME in $CHART_NAMES; do
+    helm delete --purge $CHART_NAME
+done
 
 # Delete the EFS stack.
 aws cloudformation delete-stack --stack-name $CUSTOMISATION_STACK_NAME
