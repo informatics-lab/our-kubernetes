e1c3600f96df6db6ed68a5c90d83030308eb6e41
Peter Killick <peter.killick@metoffice.gov.uk>
2019-03-26 11:00:25 +0000

    Add aws iam authenticator

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -12,6 +12,11 @@ curl -LO https://storage.googleapis.com/kubernetes-release/release/$KUBECTL_RELE
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
+# Install aws-iam-authenticator
+curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.11.5/2018-12-06/bin/linux/amd64/aws-iam-authenticator
+chmod +x ./aws-iam-authenticator
+sudo mv ./aws-iam-authenticator /usr/local/bin/aws-iam-authenticator
+
 # Install aswcli
 pip3 install awscli --upgrade
 
