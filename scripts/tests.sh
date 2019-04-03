03572f32d4e47517b117988ba26af67826c73e69
Peter Killick <peter.killick@metoffice.gov.uk>
2019-04-03 15:31:57 +0100

    Add more testing

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -13,5 +13,15 @@ eksctl get cluster
 # List nodegroups
 eksctl get ng --cluster=$(eksctl get cluster | grep -iv name | grep -iv no | awk '{ print $1 }' | head -n 1)
 
+# Check all pods are running.
+ALL_PODS=$(kubectl get pods --all-namespaces | grep -iv namespace)
+N_TOTAL_PODS=$(echo $ALL_PODS | wc -l)
+N_RUNNING_PODS=$(echo $ALL_PODS | grep -i running | wc -l)
+
+if [ $N_TOTAL_PODS -ne $N_RUNNING_PODS ]; then
+    echo "Not all pods are running!"
+    exit 1
+fi
+
 # Done!
 echo "### We gone tested yo' thing, sir. ###"
