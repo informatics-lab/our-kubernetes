9fa0080e07cde5e4edabee80cbeb1b64a1fdb2e9
Peter Killick <peter.killick@metoffice.gov.uk>
2019-07-22 14:41:15 +0100

    Use shadow resource group for storage account check

--- azure/bin/create_cluster_storage.sh
+++ azure/bin/create_cluster_storage.sh
@@ -14,15 +14,16 @@ set -ex
 # Get kubernetes credentials for AKS resource.
 az aks get-credentials -g $CLUSTER_GROUP_NAME -n $RESOURCE_NAME --overwrite-existing
 
+# XXX: The storage account needs to be created in the "shadow resource group".
+#      See https://github.com/Azure/AKS/issues/91.
+SHADOW_RG_NAME="MC_${CLUSTER_GROUP_NAME}_${RESOURCE_NAME}_${RESOURCE_LOCATION}"
+
 # Check for an existing azure storage account for Pangeo, and create it if missing.
 EXISTING_SA_NAMES=$(az storage account list \
-                      --resource-group $CLUSTER_GROUP_NAME \
+                      --resource-group $SHADOW_RG_NAME \
                       --query "[].name | join(',', @)")
 
 if [[ ! $EXISTING_SA_NAMES =~ $STORAGE_ACCT_NAME ]]; then
-    # XXX: The storage account needs to be created in the "shadow resource group".
-    #      See https://github.com/Azure/AKS/issues/91.
-    SHADOW_RG_NAME="MC_${CLUSTER_GROUP_NAME}_${RESOURCE_NAME}_${RESOURCE_LOCATION}"
     az storage account create \
         --name $STORAGE_ACCT_NAME \
         --resource-group $SHADOW_RG_NAME \
